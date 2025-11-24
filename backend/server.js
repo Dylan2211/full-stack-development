ef98d58313002b4d0c0e8a66db76f1d8a90eecd2
Dylan <dylan.wongyk@gmail.com>
2025-11-24 10:37:53 +0800

    login paths

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
-// const taskRoutes = require("./routes/taskRoutes");
-const taskRoutes = require("./routes/no_login_routes");
+const taskRoutes = require("./routes/taskRoutes");
+const no_login_routes = require("./routes/no_login_routes");
 const app = express();
 
 dotenv.config();
@@ -11,16 +11,21 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/tasks", taskRoutes);
+app.use("/tasks", no_login_routes);
 app.use(express.static(path.join(__dirname, "../frontend")));
+app.use("/api", taskRoutes);
 
 app.get("/", (req, res) => {
   res.redirect("/test");
 });
 app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "test", "../frontend/test/task.html"));
+  res.sendFile(path.join(__dirname, "../frontend/test/task.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/login/login.html"));
 });
 
+// #region Error handling middleware
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
@@ -32,3 +37,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
+// #endregion
